84a2b9e68c302c14a94746be23f58834c6a8198d
faridkhan5 <faridkhan13555@gmail.com>
2024-09-18 00:03:59 +0530

    court keypoints detection

---
+++ court_line_detector/court_line_detector.py
@@ -0,0 +1,57 @@
+import torch
+from torchvision import models, transforms
+import cv2
+
+
+class CourtLineDetector:
+    def __init__(self, model_path):
+        self.model = models.resnet50(weights=False)
+        # modify fc layer
+        self.model.fc = torch.nn.Linear(self.model.fc.in_features, 14*2)
+        # load saved wts
+        self.model.load_state_dict(torch.load(model_path, map_location='cpu'))
+
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),  # (C, H, W) -> (3,224,224)
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+    def predict(self, image):
+        """ predicts the keypoints using the loaded model
+        Returns:
+            1-D np arr: x,y coordinates of all 14 keypoints
+        """
+        # only predict on the 1st frame, because camera remains stationary
+            # - tennis court remains static across all frames
+        img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # model needs inp in a list format for pred
+        img_tensor = self.transform(img_rgb).unsqueeze(0) # img -[unsqueeze]-> [img]
+        with torch.no_grad():
+            outputs = self.model(img_tensor)  # (1,28)
+        keypoints = outputs.squeeze().cpu().numpy()  # (28)
+
+        # map back transformed img to original img dims
+        og_h, og_w = img_rgb.shape[:2]
+        keypoints[::2] *= og_w / 224.0
+        keypoints[1::2] *= og_h / 224.0
+
+        return keypoints  # [x1, y1, x2, y2, x1, y1, x2, y2, ...]
+    
+    def draw_keypoints(self, image, keypoints):
+        for i in range(0, len(keypoints), 2):
+            x = int(keypoints[i])
+            y = int(keypoints[i+1])
+
+            # draw dot on the coords
+            cv2.putText(image, str(i//2), (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+            cv2.circle(image, (x,y), 5, (0,0,255), -1)  # -1 -> filled circle
+        return image
+    
+    def draw_keypoints_on_video(self, video_frames, keypoints):
+        output_video_frames = []
+        for frame in video_frames:
+            frame = self.draw_keypoints(frame, keypoints)
+            output_video_frames.append(frame)
+        return output_video_frames
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils import (read_video,
                    save_video)
 from trackers import PlayerTracker, BallTracker
-
+from court_line_detector import CourtLineDetector
 
 def main():
     # read video
@@ -18,12 +18,19 @@ def main():
     ball_detections = ball_tracker.detect_frames(video_frames,
                                                 read_from_stub=True,
                                                 stub_path="tracker_stubs/ball_detections.pkl")
+    
+    # courtline detector model
+    court_model_path = "models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
 
     # draw output
     ## draw player bbox
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
     ## draw ball bbox
     output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
+    ## draw court keypoints
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
 
     # save video
     save_video(output_video_frames, "output_videos/output_video.avi")
